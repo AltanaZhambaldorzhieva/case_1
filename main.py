61656b9f530ae0b459407537efa90a6a27f70dba
AltanaZhambaldorzhieva <zhambaldorzhieva2005@mail.ru>
2024-02-13 20:32:05 +0700

    case1

--- main.py
+++ main.py
@@ -1,3 +1,57 @@
 # Case_1
 # Zhambaldorzhieva A., Ryaguzova D., Zaytseva D.
 #
+import turtle
+def paral(x,y,a):
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.down()
+    turtle.color('purple')
+    turtle.begin_fill()
+    turtle.forward(a)
+    turtle.right(45)
+    turtle.forward(a / 2)
+    turtle.right(135)
+    turtle.forward(a)
+    turtle.right(45)
+    turtle.forward(a / 2)
+    turtle.right(135)
+    turtle.end_fill()
+    turtle.done()
+
+def square(x, y, a):
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.down()
+    turtle.color('red', 'red')
+    turtle.begin_fill()
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.end_fill()
+    turtle.done()
+
+def triangle(x, y, a, linecolor, color):
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.down()
+    turtle.color(linecolor, color)
+    turtle.begin_fill()
+    hyp = a * 2 ** 0.5
+    turtle.forward(a)
+    turtle.left(135)
+    turtle.forward(hyp)
+    turtle.left(135)
+    turtle.forward(a)
+    turtle.end_fill()
+    turtle.left(90)
+    turtle.done()
+
+
+paral(1,1,100)
+
